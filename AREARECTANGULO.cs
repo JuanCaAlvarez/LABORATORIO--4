71364ebc923a12c6f654e0b74f844def6e28b2c5
JUAN CARLOS ALVAREZ A <jeici0507@gmail.com>
2025-11-15 20:47:42 -0500

    Add files via upload

---
+++ AREARECTANGULO.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program { 
+    static double CalcularArea(double baseR, double altura)
+    {
+        return baseR * altura;
+    }
+    
+    static void Main()
+    {
+        Console.Write("Ingrese la base: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("Ingrese la altura: ");
+        double h = double.Parse(Console.ReadLine());
+
+        double area = CalcularArea(b,h);
+        Console.WriteLine($"El área del rectángulo es: {area}");
+
+    }
+
+}
